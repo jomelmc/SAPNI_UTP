0bb71195eddb10865ee9699ac5ffdb1ba557d915
Christopher Jiménez <christopher.jimenez@utp.ac.pa>
2018-10-28 00:06:22 -0600

    Correcciones

--- BD_SAPNI/BD_SAPNI.sql
+++ BD_SAPNI/BD_SAPNI.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-10-2018 a las 08:29:51
+-- Tiempo de generación: 28-10-2018 a las 07:03:30
 -- Versión del servidor: 10.1.34-MariaDB
 -- Versión de PHP: 7.2.8
 
