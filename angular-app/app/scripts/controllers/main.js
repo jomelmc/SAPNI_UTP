c6aa2bb6693051a48f1272c6301a66dd72964017
Christopher Jiménez <christopher.jimenez@utp.ac.pa>
2018-11-16 01:43:12 -0600

    Se agregó dummy para login y nuevo enrutamiento de estados

--- angular-app/app/scripts/controllers/main.js
+++ angular-app/app/scripts/controllers/main.js
@@ -8,10 +8,30 @@
  * Controller of the angularAppApp
  */
 angular.module('angularAppApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($state) {
+
+    /** SE DECLARA EL ÁMBITO GLOBAL */
+    var vm = this;
+
+    /** INICIALIZAR VALORES */
+    // temporal dummy
+    var id = '8-8-8';
+    var password = '12345678';
+
+    /** FUNCIONES */
+    vm.loginOnSubmit = function () {
+      if (angular.isDefined(vm.user.id) && angular.isDefined(vm.user.password) && vm.user.id == id && vm.user.password == password) {
+        $state.go("solicitud_apoyo_economico");
+      }
+    };
+
+    vm.setup = function () {
+
+      vm.user = {
+        entityId: '3425',
+        entityType: 'ESTUDIANTE',
+      };
+    };
+
+    vm.setup();
   });
